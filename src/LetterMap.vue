0532794a884e8dff8083ce2db0bbf6557d7ea804
Millie <milliedarc@gmail.com>
2025-06-15 22:02:12 +0100

    Forgot to commit the LetterMap

---
+++ src/LetterMap.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="grid">
+    <div class="tile">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.grid {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+}
+
+.tile {
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  border: 2px solid black;
+}
+
+</style>
